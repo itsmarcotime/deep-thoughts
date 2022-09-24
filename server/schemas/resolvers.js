3f749a6e1009b72b1a7001d2819f2738833b6bb1
Marco <patrickstar1281@yahoo.com>
2022-09-24 10:57:51 -0500

    set up queries on apllo using graphql

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,38 @@
+const { User, Thought } = require('../models');
+
+const resolvers = {
+
+  Query: {
+
+    //get all thoughts
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+
+    //gets a single thought
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
+
+    //gets all users
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+
+    // get a user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    }
+
+  }
+
+};
+
+module.exports = resolvers;
